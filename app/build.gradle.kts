54cee2fa0bf0e82cbeb3983f9c8e04959ac58961
Jean Alvarez <jeanco19.ja@gmail.com>
2024-01-01 19:32:38 -0400

    Added version catalog to manage the project dependencies.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,18 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.kapt)
+    alias(libs.plugins.hilt.android)
 }
 
 android {
     namespace = "com.jean.moviesarchitectcoders"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.jean.moviesarchitectcoders"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -37,11 +39,37 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    // DEFAULT
+    implementation(libs.core.ktx)
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.constraintlayout)
+
+    // COROUTINES
+    implementation(libs.bundles.coroutines)
+
+    // RETROFIT
+    implementation(libs.bundles.retrofit)
+
+    // DAGGER HILT
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.android.compiler)
+
+    // ROOM DB
+    implementation(libs.bundles.room.database)
+    kapt(libs.room.compiler)
+
+    // COIL
+    implementation(libs.coil.kt)
+
+    // TEST
+    testImplementation(libs.junit)
+
+    // ANDROID TEST
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
+
+kapt {
+    correctErrorTypes = true
 }
